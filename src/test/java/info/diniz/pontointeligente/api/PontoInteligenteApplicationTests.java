aa11acb495483db07f77279cd423edcb9f88db78
harleydiniz <fharley@gmail.com>
2018-06-03 14:12:51 -0300

    Commit inicial dos arquivos do projeto.

---
+++ src/test/java/info/diniz/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,16 @@
+package info.diniz.pontointeligente.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PontoInteligenteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
