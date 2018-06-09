102dfe34db9c7fb60228e267becae0cb8dadef63
harleydiniz <fharley@gmail.com>
2018-06-08 23:52:46 -0300

    Adicionado serviço de Empresa e Funcionário.

---
+++ src/test/java/info/diniz/pontointeligente/api/utils/PasswordUtilsTest.java
@@ -0,0 +1,26 @@
+package info.diniz.pontointeligente.api.utils;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtilsTest {
+
+	private static final String SENHA = "123456";
+	
+	private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+	
+	@Test
+	public void testSenhaNula() throws Exception {
+		assertNull(PasswordUtils.geraBCrypt(null));
+	}
+	
+	@Test
+	public void testGerarHashSenha() throws Exception {
+		String hash = PasswordUtils.geraBCrypt(SENHA);
+		assertTrue(encoder.matches(SENHA, hash));
+	}
+	
+}
